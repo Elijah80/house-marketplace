f8f5f6f00827060e8fbea82d05a3e7b380899291
Elijah Cravens <elijahcravens46@gmail.com>
2024-02-20 13:06:36 -0700

    Removed a hook that was not being used. Created Category page and added a route for it, based on if the item is for sale or for rent. Added logic to fetch the listings from the database and display them on the Category page.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 function Explore() {
-  return (
-    <div>
-      <h1>Explore</h1>
-    </div>
-  )
+	return (
+		<div className='explore'>
+			<header>
+				<p className='pageHeader'>Explore</p>
+			</header>
+
+			<main>
+				{/* TODO: Add Slider */}
+				<p className='exploreCategoryHeading'>Categories</p>
+				<div className='exploreCategories'>
+					<Link to='/category/rent'>
+						<img src={rentCategoryImage} alt='For Rent' className='exploreCategoryImg' />
+            <p className="exploreCategoryName">Places for Rent</p>
+					</Link>
+          <Link to='/category/sale'>
+						<img src={sellCategoryImage} alt='For Sale' className='exploreCategoryImg' />
+            <p className="exploreCategoryName">Places for Sale</p>
+					</Link>
+				</div>
+			</main>
+		</div>
+	)
 }
 
 export default Explore
