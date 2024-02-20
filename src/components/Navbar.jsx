b74d2ffab9f141d838bc1c88a4802a4a0eb63cef
Elijah Cravens <elijahcravens46@gmail.com>
2024-02-20 14:04:01 -0700

    Added content to the Offers page to show listings that have an offer on them.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
 
 function Navbar() {
-  const navLinks = [
-    { path: '/', icon: <ExploreIcon />, text: 'Explore'},
-    { path: '/offers', icon: <OfferIcon />, text: 'Offer'},
-    { path: '/profile', icon: <PersonOutlineIcon />, text: 'Profile'}
-  ]
+	const navLinks = [
+		{ path: '/', icon: <ExploreIcon />, text: 'Explore' },
+		{ path: '/offers', icon: <OfferIcon />, text: 'Offers' },
+		{ path: '/profile', icon: <PersonOutlineIcon />, text: 'Profile' },
+	]
 	const navigate = useNavigate()
 	const location = useLocation()
 
@@ -23,57 +23,22 @@ function Navbar() {
 		<footer className='navbar'>
 			<nav className='navbarNav'>
 				<ul className='navbarListItems'>
-          {navLinks.map(({ path, icon: Icon, text }) => (
-            <li key={path} className='navbarListItem' onClick={() => navigate(path)}>
-              {React.cloneElement(Icon, {
-                fill: pathMatchRoute(path) ? '#2c2c2c' : '#8f8f8f',
-                width: '36px',
-                height: '36px'
-              
-              })}
-              <p className={pathMatchRoute(path) ? 'navbarListItemNameActive' : 'navbarListItemName'}>
-                {text}
-              </p>
-            </li>
-          ))}
-					{/* <li className='navbarListItem' onClick={() => navigate('/')}>
-						<ExploreIcon
-							fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
-							width='36px'
-							height='36px'
-						/>
-						<p className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>
-							Explore
-						</p>
-					</li>
-					<li className='navbarListItem' onClick={() => navigate('/offers')}>
-						<OfferIcon
-							fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
-							width='36px'
-							height='36px'
-						/>
-						<p
-							className={
-								pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'
-							}
-						>
-							Offer
-						</p>
-					</li>
-					<li className='navbarListItem' onClick={() => navigate('/profile')}>
-						<PersonOutlineIcon
-							fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
-							width='36px'
-							height='36px'
-						/>
-						<p
-							className={
-								pathMatchRoute('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'
-							}
-						>
-							Profile
-						</p>
-					</li> */}
+					{navLinks.map(({ path, icon: Icon, text }) => (
+						<li key={path} className='navbarListItem' onClick={() => navigate(path)}>
+							{React.cloneElement(Icon, {
+								fill: pathMatchRoute(path) ? '#2c2c2c' : '#8f8f8f',
+								width: '36px',
+								height: '36px',
+							})}
+							<p
+								className={
+									pathMatchRoute(path) ? 'navbarListItemNameActive' : 'navbarListItemName'
+								}
+							>
+								{text}
+							</p>
+						</li>
+					))}
 				</ul>
 			</nav>
 		</footer>
